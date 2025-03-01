2c0da39f14f90083433bd48e95e17fee4c0deba3
Arthur Simon <arvinsim@users.noreply.github.com>
2025-03-02 00:31:20 +0800

    Initial beautification of the terminal (#28)
    * Initial code to beautify the terminal
    
    * Squashed commit of the following:
    
    commit 59bd27085d13047ed90266d343ab263c3ce6ccab
    Merge: 023abd6 37bbbff
    Author: Arthur Simon <arvinsim@users.noreply.github.com>
    Date:   Sat Mar 1 15:58:26 2025 +0800
    
        Merge pull request #27 from arvinsim/feat/26-setup-unit-testing
    
        Setup initial unit testing
    
    commit 37bbbffb993972ff3faaa53573bca066b331e631
    Author: Arthur Vincent Simon <developarvin@gmail.com>
    Date:   Sat Mar 1 15:56:19 2025 +0800
    
        Add test for user_repository
    
    commit e8c7610245dffec817ce7c0910d5294165e9835a
    Author: Arthur Vincent Simon <developarvin@gmail.com>
    Date:   Sat Mar 1 15:50:44 2025 +0800
    
        Add packages
    
    * Fix go mod

--- cmd/game-reviews/main.go
+++ cmd/game-reviews/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/arvinsim/game-reviews-api/internal/handlers"
 	"github.com/arvinsim/game-reviews-api/internal/repository"
 	"github.com/arvinsim/game-reviews-api/internal/service"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/joho/godotenv"
 )
 
@@ -38,14 +39,41 @@ func main() {
 	gameHandler := &handlers.GameHandler{}
 	reviewHandler := &handlers.ReviewHandler{}
 
-	mux.HandleFunc("GET /users", userHandler.GetUsers)
+	mux.HandleFunc("GET /users", corsMiddleware(userHandler.GetUsers))
 	mux.HandleFunc("POST /users", userHandler.CreateUser)
 	mux.HandleFunc("GET /games", gameHandler.GetGames)
 	mux.HandleFunc("GET /reviews", reviewHandler.GetReviews)
 
 	// Run the server
-	fmt.Println("Server is running on port 8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	runServer(8080, mux)
+}
+
+func runServer(port int, mux *http.ServeMux) {
+	// Run the server
+	message := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#FAFAFA")).
+		Background(lipgloss.Color("#7D56F4")).
+		Padding(0, 2)
+
+	fmt.Printf(message.Render("Server is running on port: %d"), port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
+	}
+}
+
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next(w, r)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.0
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/charmbracelet/lipgloss v1.0.0
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/stretchr/testify v1.10.0
@@ -15,4 +16,13 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/x/ansi v0.4.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 )
